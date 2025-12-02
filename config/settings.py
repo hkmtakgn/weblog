4d9c4240a8ab18919cfb80a784a7ee6974680887
hkmtakgn <hikmetakgun94@gmail.com>
2025-12-02 23:52:31 +0300

    'edited-endpoint'

--- config/settings.py
+++ config/settings.py
@@ -41,13 +41,13 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_ACCESS_KEY_ID = DIGITAL_OCEAN_KEY_ID
 AWS_SECRET_ACCESS_KEY = DIGITAL_OCEAN_SECRET_KEY
 AWS_STORAGE_BUCKET_NAME = "weblogbucket"
-AWS_S3_ENDPOINT_URL = "https://weblogbucket.nyc3.digitaloceanspaces.com"
+AWS_S3_ENDPOINT_URL = "https://nyc3.digitaloceanspaces.com"
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
 AWS_LOCATION = "media_sub_dir"
 
-MEDIA_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.nyc3.digitaloceanspaces.com/{AWS_LOCATION}/"
+MEDIA_URL = f"https://{AWS_S3_ENDPOINT_URL}/{AWS_STORAGE_BUCKET_NAME}/{AWS_LOCATION}/"
 MEDIA_ROOT = os.path.join(BASE_DIR,"mediafiles")
 
 # ------------------------
