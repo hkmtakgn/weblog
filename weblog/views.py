c55ff4d20ddba8afa71f66e2990d9befc0bff906
hkmtakgn <hikmetakgun94@gmail.com>
2025-12-02 23:05:35 +0300

    'added-pro-card-to-home-page'

--- weblog/views.py
+++ weblog/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from weblog.models import Product
 
 def weblog_home_views (request):
-    context = dict ()
+    products = Product.objects.all()
+    context = dict (
+        products=products,
+    )
     return render (request, "main_pages/weblog-home.html", context)
 
 
